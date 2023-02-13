8d45cca801933810f831aa1ab7d644dfa3cc9df2
Praveen Venkatesh <30774147+praveenVnktsh@users.noreply.github.com>
2023-02-12 20:32:16 -0500

    done

--- Kinematics/MainTest.py
+++ Kinematics/MainTest.py
@@ -7,22 +7,22 @@ mybot=Franka.FrankArm()
 # Compute forward kinematics
 deg_to_rad = np.pi/180.
 
-joint_targets = [[0., 0., 0., 0., 0., 0., 0.], \
-                 [0, 0, -45.*deg_to_rad,-15.*deg_to_rad,20.*deg_to_rad,15.*deg_to_rad,-75.*deg_to_rad], \
-                 [0, 0, 30.*deg_to_rad, -60.*deg_to_rad,-65.*deg_to_rad,45.*deg_to_rad,0.*deg_to_rad],\
-		            ]
+# joint_targets = [[0., 0., 0., 0., 0., 0., 0.], \
+#                  [0, 0, -45.*deg_to_rad,-15.*deg_to_rad,20.*deg_to_rad,15.*deg_to_rad,-75.*deg_to_rad], \
+#                  [0, 0, 30.*deg_to_rad, -60.*deg_to_rad,-65.*deg_to_rad,45.*deg_to_rad,0.*deg_to_rad],\
+# 		            ]
 
-for joint_target in joint_targets:
-  print ('\nJoints:')
-  print (joint_target)
-  Hcurr,J =mybot.ForwardKin(joint_target)
-  ee_pose = Hcurr[-1]
-  rot_ee = ee_pose[:3,:3]
-  pos_ee = ee_pose[:3,3]
-  print('computed FK ee position')
-  print(pos_ee)
-  print('computed FK ee rotation')
-  print(rot_ee)
+# for joint_target in joint_targets:
+#   print ('\nJoints:')
+#   print (joint_target)
+#   Hcurr,J =mybot.ForwardKin(joint_target)
+#   ee_pose = Hcurr[-1]
+#   rot_ee = ee_pose[:3,:3]
+#   pos_ee = ee_pose[:3,3]
+#   print('computed FK ee position')
+#   print(pos_ee)
+#   print('computed FK ee rotation')
+#   print(rot_ee)
 
 
 # Compute inverse kinematics
